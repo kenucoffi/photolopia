ff5e91983bae0a80cd7dc3a7641b7d65d1b09f53
coffikenu <kenisatetemke@gmail.com>
2026-05-17 16:05:38 +0300

    photolopia project

---
+++ app/Http/Controllers/API/V1/GetPostController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\GetPostResource;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Faker\Core\File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class GetPostController extends Controller
+{
+    //
+    public function show()
+    {
+        //
+        $user = Auth::user();
+        if($user){
+            $id = $user->id;
+            $post = Post::where("user_id","=",$id)->get();
+            return GetPostResource::collection($post);
+        }
+        else{
+            return response(["message"=>"unauthorized"],401);
+        }
+
+    }
+    public function public_show(string $id){
+        $post = Post::where("user_id","=",$id)->get();
+        return GetPostResource::collection($post);
+    }
+     public function index()
+    {
+        //
+        $post = Post::all();
+        return PostResource::collection($post);
+    }
+
+}
